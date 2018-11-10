b6f9fdb5573922a7c582bffd238f2206f8ad7de4
denibulkashvili <denibulkashvili@gmail.com>
2018-11-10 13:53:21 +0700

    Register group model in admin

--- groups/admin.py
+++ groups/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from . import models
+
+
+class GroupMemeberInline(admin.TabularInline):
+    model = models.GroupMember
 
 # Register your models here.
+admin.site.register(models.Group)
