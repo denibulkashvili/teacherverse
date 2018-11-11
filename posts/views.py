82e9c4ae13f8d87ead9df1e3b1b774cf22127243
denibulkashvili <denibulkashvili@gmail.com>
2018-11-11 23:50:33 +0700

    Add  basic view for posts app

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+from django.http import Http404
+
+from braces.views import SelectRelatedMixin
+
+from django.views import generic
+
+from . import models
+from . import forms
+
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
 # Create your views here.
+class PostList(SelectRelatedMixin, generic.list.ListView):
+    model = models.Post
+    select_related = ('user', 'group')
+
