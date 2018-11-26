6ef9b5e78854d17204cd07df30035e6200191821
denibulkashvili <denibulkashvili@gmail.com>
2018-11-26 21:24:48 +0700

    Add Create POst and Delete Post views

--- posts/views.py
+++ posts/views.py
@@ -42,7 +42,29 @@ class PostDetail(SelectRelatedMixin, generic.DetailView):
 
     def get_queryset(self):
         queryset = super().get_queryset()
-        return queryset.filter()
+        return queryset.filter(user__username__iexact = self.kwargs.get('username'))
 
+class CreatePost(LoginRequiredMixin, SelectRelatedMixin, generic.CreateView):
+    fields = ('message', 'group')
+    model= models.Post
 
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.user = self.request.user
+        self.object.save()
+        return super().form_valid(form)
+
+class DeletePost(LoginRequiredMixin, SelectRelatedMixin, generic.DeleteView):
+    model = models.Post
+    select_related = ('user', 'group')
+    success_url = reverse_lazy('posts:all')
+
+    def def get_queryset(self):
+        quertset = super().get_queryset()
+        return queryset.filter(user_id = self.request.user.id)
+
+    def delete(self, *args, **kwargs):
+        messages.success(self.request, "Post Deleted")
+        return super().delete(*args, **kwargs)
+     
 
